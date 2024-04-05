d88405524bb2ed3c550a72be2df94397bdc293c6
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-05 15:06:43 +0100

    started

---
+++ part1.py
@@ -0,0 +1,19 @@
+# Definición de la función get_choices()
+def get_choices():
+    # Asignación de valores a las variables
+    player_choice = "rock"
+    computer_choice = "paper"
+    
+    # Devolver el valor de computer_choice
+    return computer_choice
+
+# Definición de la función greeting()
+def greeting():
+    # Devolver un saludo
+    return "Hi"
+
+# Llamada a la función greeting() y almacenamiento del resultado en la variable response
+response = greeting()
+
+# Imprimir el resultado almacenado en response
+print(response)
