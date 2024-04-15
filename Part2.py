079d6257b5cc18afeeccde0a50aaf56d4aeb7439
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-15 10:06:03 +0100

    1hour

--- Part2.py
+++ Part2.py
@@ -2,10 +2,10 @@
 # lets create a new function called check 
 # Functions can receive data they are called the data. 
 
-def check_win(player , computer ):
- print ("You Chose" + player + ", computer chose" + computer)
- if player == computer:
-   return "it is a tie!"
+#def check_win(player , computer ):
+ #print ("You Chose" + player + ", computer chose" + computer)
+ #if player == computer:
+ #  return "it is a tie!"
 # RPS if stataments
  
 # one == is the assignment operator are the same value 
@@ -19,3 +19,36 @@ def check_win(player , computer ):
 
 #Refactoring and nested if is the process of restructuring code while keeping the original functionality 
 #when creating a program it is common to refactor 
+
+# Accessing dictionary values 
+
+#VARIABLES 
+#name = "Michael" 
+
+#age = "24" 
+
+#name1
+#HEIGHT 
+#my_name 
+#__name
+
+# Expression and statements  
+
+name = "Michael" ; print(name) 
+
+#Comments 
+
+#this is a commented line 
+
+name = "Michael" #this is a inline comment 
+print(name)
+
+#Data types 
+
+name = "Michael"
+print(isinstance(name, str))
+
+
+number = "20"
+age = int("number")
+print(isinstance(age , int))
