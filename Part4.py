7e777382b87402fd9b04c039b9ae1bbf7303e499
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-19 10:52:24 +0100

    add

---
+++ Part4.py
@@ -0,0 +1,29 @@
+## capital letter 
+
+name = "Michael"
+print(name.lower())
+print(len(name))
+
+## escaping characters is a way to add special characters into a string
+# for example we want add the double quote into a double quote 
+
+name = "Micha\"el"
+
+print(name) 
+
+## String characters & slicing 
+
+name = "Michael is cool "
+
+print(name[1:7]) 
+
+# Booleans #bool type and this can have two values true or false 
+
+done = False
+
+if done: 
+    print("yes")
+
+else: 
+    print("no")
+
---
+++ part5.py
@@ -0,0 +1,10 @@
+## number data types 
+
+num1 = 2+3j
+num2 = complex(2,3)
+
+print(num2.real, num2.imag)
+
+#Build in functions 
+
+print(round(-5.5))
