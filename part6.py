fab2f9f4f5e01118a28d4451a55a1a73f5fd5271
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-20 18:19:01 +0100

    add

---
+++ part6.py
@@ -0,0 +1,19 @@
+## User input 
+print("what is your age")
+age = input()
+print("Your age is" + age)
+
+## Control statments 
+
+condition = True 
+
+if condition == True : 
+    print("The condition")
+    print("was true")
+
+# List are essential python data structure 
+dogs = ["Roger" ,1,  "Syd", True ] 
+dogs[2] = "Beau" ## we update en el lugar syd like a operator
+print(dogs[2])
+
+
---
+++ part7.py
@@ -0,0 +1,12 @@
+## dictionaries are another very important python data structure 
+dog = { "name" : "Roger" , "age" : 8 , 
+       "color": "green"}   
+
+
+dog["name"] = "Syd"
+
+
+
+print(dog.popitem()) 
+
+print(dog)
